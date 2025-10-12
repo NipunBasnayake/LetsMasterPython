81ac4c7a3a62b46279f19e7466cd03d5aa4e1dc4
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-12 23:27:59 +0530

    explore basic data types

---
+++ 4_data_types.py
@@ -0,0 +1,10 @@
+number = 42             # Integer
+floating_point = 3.14   # Float
+text = "Hello, World!"  # String
+boolean = True          # Boolean
+
+print (type(number))            # <class 'int'>
+print (type(floating_point))    # <class 'float'>
+print (type(text))              # <class 'str'>
+print (type(boolean))           # <class 'bool'>
+
