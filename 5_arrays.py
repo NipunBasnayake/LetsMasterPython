9facfd8add82e89e9c8ebe92fc5c9499cb5e299b
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-12 23:35:10 +0530

    explore arrays

---
+++ 5_arrays.py
@@ -0,0 +1,16 @@
+my_list = [1, 2, 3, 4, 5]       # Create a list
+
+first_element = my_list[0]      # Access the first element
+
+subste = my_list[1:4]           # Slicing from index 1 to 3
+
+my_list[2] = 10                 # Modify the third element 
+
+my_list.append(6)               # Append an element to the end
+
+my_list.remove(2)               # Remove the element with value 2
+
+print(f"my_list: {my_list}")                # Output the modified list
+print(f"first_element: {first_element}")    # Output the first element
+print(f"subste: {subste}")                  # Output the sliced list
+
