b680abc447ca3dd3d53b13e5d69c34825c1a33e0
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-12 23:38:40 +0530

    using numpy to arrays

--- 5_arrays.py
+++ 5_arrays.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 my_list = [1, 2, 3, 4, 5]       # Create a list
 
 first_element = my_list[0]      # Access the first element
@@ -14,3 +16,9 @@ print(f"my_list: {my_list}")                # Output the modified list
 print(f"first_element: {first_element}")    # Output the first element
 print(f"subste: {subste}")                  # Output the sliced list
 
+
+my_array = np.array([1, 2, 3, 4, 5])    # Create a NumPy array
+first_element_array = my_array[0]       # Access the first element
+
+print(f"my_numpy_array: {my_array}")                      # Output the NumPy array
+print(f"first_element_array: {first_element_array}")      # Output the first element of the array
