cc708cd2703db164fac72b9b7e56e82430428fb6
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-13 10:27:40 +0530

    Logical Operators

--- basics/7_operators.py
+++ basics/7_operators.py
@@ -1,3 +1,4 @@
+# ------------ Arithmatic Operators ------------
 a = 10
 b = 5
 
@@ -21,4 +22,17 @@ equal_to = x == y
 not_equal_to = x != y
 
 print("equal_to: ", equal_to)
-print("not_equal_to", not_equal_to)
+print("not_equal_to: ", not_equal_to)
+
+# ------------ Logical Operators ------------
+p = True
+q = False
+
+logical_and = p and q
+logical_or = p or q
+logical_not = not p
+
+print("logical_and: ", logical_and)
+print("logical_or: ", logical_or)
+print("logical_not: ", logical_not)
+
