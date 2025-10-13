17ee4c9d773475f461101a8715054f9bcf7b9708
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-13 14:22:21 +0530

    Explore Tuples Methods

---
+++ 5_tuples/3_tuple_methods.py
@@ -0,0 +1,19 @@
+# 1. Counting occurrences of an element in a tuple
+my_tuple_1 = (1, 2, 2, 3, 4, 2, 5)
+count_of_2 = my_tuple_1.count(2)
+print("Count of 2:", count_of_2)
+
+# 2. Finding the index of an element
+my_tuple_2 = (10, 20, 30, 40, 50)
+index_of_30 = my_tuple_2.index(30)
+print("Index of 30:", index_of_30)
+
+# 3. Sorting a tuple (returns a list)
+my_tuple_3 = (5, 3, 7, 8, 6, 2, 4, 1)
+sorted_tuple = sorted(my_tuple_3)
+print("Sorted tuple:", sorted_tuple)
+
+# 4. Getting the length of a tuple
+my_tuple_4 = (40, 20, 10, 30, 50, 70)
+length_of_tuple = len(my_tuple_4)
+print("Length of tuple:", length_of_tuple)
