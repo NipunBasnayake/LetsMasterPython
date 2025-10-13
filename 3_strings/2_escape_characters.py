2a77fca3fd1066774dd8b49483a0e07c6306fb63
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-13 12:15:30 +0530

    Explore Scape Characters

---
+++ 3_strings/2_escape_characters.py
@@ -0,0 +1,17 @@
+# ------------- example 01 -------------
+message = "This is a line\nThis is a new line"
+
+print("Original message")
+print(message)
+
+# ------------- example 02 -------------
+special_message = "This is a tab\tThis is a backslash \\"
+
+print("Special message")
+print(special_message)
+
+# ------------- example 03 -------------
+symbol_text = "This is the euro symbol: \u20AC"
+
+print("Symbol text")
+print(symbol_text)
