0fc8594c16e462db64e87178d416baee4c1fefbc
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-13 12:07:43 +0530

    Explore String Formatting

---
+++ 3_strings/1_string_formating.py
@@ -0,0 +1,24 @@
+# ------------- example 01 -------------
+name = "Python"
+version = "3.9"
+formatted_string = "Welcome to {} {}!".format(name, version)
+
+print("Name:", name)
+print("Version:", version)
+print("Formatted string:", formatted_string)
+
+# ------------- example 02 -------------
+course = "Python Programming"
+hours = 8
+course_info = "Course: {0} \nDuration: {1} hours".format(course, hours)
+
+print("Course:", course)
+print("Hours:", hours)
+print("Course information:", course_info)
+
+# ------------- example 03 -------------
+pi_value = 3.14159265359
+formatted_pi = "value of pi: {:.2f}".format(pi_value)
+
+print("Original pi value:", pi_value)
+print("Formatted pi value:", formatted_pi)
--- variables/1_output_variables.py
+++
@@ -1,8 +0,0 @@
-name = "Smith John"
-age = 30
-
-print("Person: " + name + ", Age: " + str(age))
-print(f"Person: {name}, Age: {age}")
-
-output = "Person: {name}, Age: {age}".format(name=name, age=age)
-print(output)
