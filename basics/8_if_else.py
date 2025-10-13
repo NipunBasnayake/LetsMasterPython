f4a1a53e2c07f2e5015eccb7d7722ea891cf09a3
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-13 10:49:31 +0530

    Explore if else elif

---
+++ basics/8_if_else.py
@@ -0,0 +1,17 @@
+age = 15
+
+if age >= 18:
+    print("You are and adult")
+else:
+    print("You are a minor")
+
+score = 75
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("D")
