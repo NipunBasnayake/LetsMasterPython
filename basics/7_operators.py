8ea7d2e695a4c560507c169f426428a9d0cf4154
Nipun Basnayake <nipunsathsara1999@gmail.com>
2025-10-13 10:18:46 +0530

    Simple operators explore

---
+++ basics/7_operators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+modulo = a % b
+
+print("addition: ", addition)
+print("subtraction: ", subtraction)
+print("multiplication: ", multiplication)
+print("division: ", division)
+print("modulo: ", modulo)
