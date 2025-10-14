f0b1142e61ade6f1f61a51c9df5b60de5f3df5d3
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-14 22:44:37 +0530

    Join Sets: intersection

--- 6_sets/2_join_sets.py
+++ 6_sets/2_join_sets.py
@@ -1,5 +1,5 @@
-set1 = {1, 2, 3}
-set2 = {3, 4, 5}
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
 
 result_set = set1.union(set2)
 print(result_set)
@@ -7,3 +7,5 @@ print(result_set)
 result_set = set1 | set2
 print(result_set)
 
+result_set = set1.intersection(set2)
+print(result_set)
