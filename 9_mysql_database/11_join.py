fced750bc22194c1158dc9e9f822d41061be8473
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-10-22 01:01:02 +0530

    MySQL Database: JOIN

--- 9_mysql_database/11_join.py
+++ 9_mysql_database/11_join.py
@@ -10,14 +10,13 @@ mydb = mysql.connector.connect(
 
 my_cursor = mydb.cursor()
 
-
 # 1. INNER JOIN (only matching rows)
 
 sql_inner = """
-SELECT customers.name, orders.product
-FROM customers
-INNER JOIN orders ON customers.id = orders.customer_id
-"""
+            SELECT customers.name, orders.product
+            FROM customers
+                     INNER JOIN orders ON customers.id = orders.customer_id \
+            """
 my_cursor.execute(sql_inner)
 inner_result = my_cursor.fetchall()
 print("INNER JOIN (Customer Orders):")
@@ -25,14 +24,13 @@ for row in inner_result:
     print(row)
 print()
 
-
 # 2. LEFT JOIN (all customers, even if no order)
 
 sql_left = """
-SELECT customers.name, orders.product
-FROM customers
-LEFT JOIN orders ON customers.id = orders.customer_id
-"""
+           SELECT customers.name, orders.product
+           FROM customers
+                    LEFT JOIN orders ON customers.id = orders.customer_id \
+           """
 my_cursor.execute(sql_left)
 left_result = my_cursor.fetchall()
 print("LEFT JOIN (All Customers with Orders if exists):")
@@ -40,14 +38,13 @@ for row in left_result:
     print(row)
 print()
 
-
 # 3. RIGHT JOIN (all orders, even if no customer - for demo)
 
 sql_right = """
-SELECT customers.name, orders.product
-FROM customers
-RIGHT JOIN orders ON customers.id = orders.customer_id
-"""
+            SELECT customers.name, orders.product
+            FROM customers
+                     RIGHT JOIN orders ON customers.id = orders.customer_id \
+            """
 my_cursor.execute(sql_right)
 right_result = my_cursor.fetchall()
 print("RIGHT JOIN (All Orders with Customer info if exists):")
@@ -55,18 +52,17 @@ for row in right_result:
     print(row)
 print()
 
-
 # 4. FULL OUTER JOIN simulation using UNION (MySQL doesn't support FULL OUTER JOIN directly)
 
 sql_full = """
-SELECT customers.name, orders.product
-FROM customers
-LEFT JOIN orders ON customers.id = orders.customer_id
-UNION
-SELECT customers.name, orders.product
-FROM customers
-RIGHT JOIN orders ON customers.id = orders.customer_id
-"""
+           SELECT customers.name, orders.product
+           FROM customers
+                    LEFT JOIN orders ON customers.id = orders.customer_id
+           UNION
+           SELECT customers.name, orders.product
+           FROM customers
+                    RIGHT JOIN orders ON customers.id = orders.customer_id \
+           """
 my_cursor.execute(sql_full)
 full_result = my_cursor.fetchall()
 print("FULL OUTER JOIN (All Customers and Orders):")
